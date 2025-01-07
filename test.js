869564787fdfba673c90d3251c81883a5da373d1
rj1112222 <92641313+rj1112222@users.noreply.github.com>
2025-01-07 13:58:49 +0000

    test,js

---
+++ test.js
@@ -0,0 +1,11 @@
+const accountId = 144741
+let accountEmail = "abc@xbox.com"
+var accountPassword = "1234"
+accountCity = "mumbai"
+let accountState
+
+// accountId = 3
+accountEmail = "bac@ril.com"
+accountPassword = "3456"
+accountCity = 'thane'
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
